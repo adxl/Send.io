0c3f81417d0ca5a45772f3d4f769ccd6187c5db0
Adel Senhadji <adelsenhadjii@gmail.com>
2020-10-28 17:30:31 +0100

    Add register page

---
+++ src/components/Register.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
+
+export default function Login() {
+	const usernameRef = useRef();
+	const passwordRef = useRef();
+
+	const handleRegister = (e) => {
+		e.preventDefault();
+
+		const data = {
+			username: usernameRef.current.value,
+			password: passwordRef.current.value,
+		};
+
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data),
+		};
+
+		fetch('http://localhost:4000/register', options).then((response) => {
+			if (response.ok) {
+				return 'signed';
+			} throw new Error('An error occured');
+		}).catch((error) => { throw error; });
+	};
+
+	return (
+		<>
+			<Container className="auth-cont d-flex align-items-center justify-content-center ">
+				<Form onSubmit={handleRegister}>
+
+					<h3 className="mb-3 text-center">Welcome to Send.io</h3>
+
+					<Form.Group controlId="formBasicEmail" className="d-flex  justify-content-center">
+						<Form.Control ref={usernameRef} type="text" placeholder="Username" required />
+					</Form.Group>
+
+					<Form.Group controlId="formBasicPassword">
+						<Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+					</Form.Group>
+
+					<Button type="submit" className="w-100">Register</Button>
+
+				</Form>
+			</Container>
+		</>
+	);
+}
