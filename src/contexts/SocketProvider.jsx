46b6103d0fdd9b9aeae61a28283169864e4f95a8
Adel Senhadji <adelsenhadjii@gmail.com>
2020-10-28 11:45:24 +0100

    Add socket

---
+++ src/contexts/SocketProvider.jsx
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import io from 'socket.io-client';
+
+const SocketContext = React.createContext();
+
+export function useSocket() {
+	return useContext(SocketContext);
+}
+
+export function Socketprovider({ id }) {
+	const [socket, setSocket] = useState();
+
+	useEffect(() => {
+		const newSocket = io(
+			'http://localhost:4000',
+			{
+				query: { id },
+				reconnection: false,
+			},
+		);
+		setSocket(newSocket);
+		return () => newSocket.close();
+	}, [id]);
+
+	return (
+
+		<SocketContext.Provider value={socket} />
+	);
+}
+
+Socketprovider.propTypes = {
+	id: PropTypes.string.isRequired,
+};
