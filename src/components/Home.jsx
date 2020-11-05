69cd64bf4d9875f1e656546c2a0e77059d258576
Adel Senhadji <adelsenhadjii@gmail.com>
2020-11-05 20:34:16 +0100

    Add URL as props

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,14 @@ import { Container } from 'react-bootstrap';
 import Login from './Login';
 import Register from './Register';
 
+const URL = 'https://send-io.herokuapp.com';
+
 export default function Home() {
 	return (
 		<>
 			<Container className="d-flex">
-				<Register />
-				<Login />
+				<Register URL={URL} />
+				<Login URL={URL} />
 			</Container>
 		</>
 	);
