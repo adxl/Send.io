e21391ccb59a4495e05b52d40aa7020adeca3542
Adel Senhadji <adelsenhadjii@gmail.com>
2020-11-06 21:16:45 +0100

    Add a feature to delete conversations

--- src/components/ConversationsSideBar.jsx
+++ src/components/ConversationsSideBar.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button, Modal } from 'react-bootstrap';
+import NewConversationModal from './NewConversationModal';
 
 const URL = 'https://send-io.herokuapp.com';
 
 export default function ConversationsSideBar() {
 	const [conversations, setConversations] = useState([]);
+	const [modalOpen, setModalOpen] = useState(false);
 
 	useEffect(() => {
 		fetch(`${URL}/conversations`, {
@@ -19,25 +21,55 @@ export default function ConversationsSideBar() {
 			.catch((error) => { throw error; });
 	}, []);
 
+	const deleteConversation = (e) => {
+		const friend = e.target.value;
+
+		const options = {
+			method: 'DELETE',
+			headers: {
+				Authorization: localStorage.getItem('send-io-usertoken'),
+				'Content-Type': 'application/json',
+			},
+		};
+
+		fetch(`${URL}/conversations/${friend}`, options)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('Error');
+				}
+			})
+			.catch((error) => { console.log(error); });
+	};
+
+	const openModal = () => {
+		setModalOpen(true);
+	};
+
+	const hideModal = () => {
+		setModalOpen(false);
+	};
+
 	return (
 		<>
 			<Container className="border">
 				<h2>Conversations</h2>
+				<Button type="button" onClick={openModal}>+</Button>
 				<div className="border ">
 					{conversations.length > 0
-						? conversations.map((c) => {
-							const { friend } = c;
-							return ((
-								<div key={friend}>
-									<p>
-										{friend}
-									</p>
-
-								</div>
-							));
-						})
+						? conversations.map((c) => (
+							<div key={c}>
+								<p>
+									{c}
+								</p>
+								<Button type="button" value={c} variant="danger" onClick={deleteConversation}>X</Button>
+							</div>
+						))
 						: <p>No conversations</p>}
 				</div>
+
+				<Modal show={modalOpen} onHide={hideModal}>
+					<NewConversationModal hideModal={hideModal} />
+				</Modal>
 
 			</Container>
 		</>
