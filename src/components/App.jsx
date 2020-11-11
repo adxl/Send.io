4b2816a3bced41a069bbf7a8952fc82d9f638736
Adel Senhadji <adelsenhadjii@gmail.com>
2020-11-11 21:20:01 +0100

    Add init fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './Home';
 import UserPictureProvider from '../contexts/UserPictureProvider';
 import Dashboard from './Dashboard';
 
+const URL = 'https://send-io.herokuapp.com';
+
 export default function App() {
 	const userToken = localStorage.getItem('send-io-usertoken');
+
+	useEffect(() => {
+		fetch(URL);
+	});
+
 	return (
 		<>
 			{ userToken
